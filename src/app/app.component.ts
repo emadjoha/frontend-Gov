3b4ce235796a044955b87a5d2ed12ec239a66113
emadjoha <emad1.2@hotmail.com>
2018-12-16 18:08:11 +0200

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,99 @@
-import { Component } from '@angular/core';
+import {
+    AfterContentChecked,
+    AfterContentInit,
+    AfterViewInit,
+    Component,
+    DoCheck,
+    OnChanges,
+    OnDestroy,
+    OnInit,
+    SimpleChanges
+} from '@angular/core';
+import {UserService} from './user.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit , OnChanges ,OnDestroy ,DoCheck
+,AfterContentInit ,AfterContentChecked,AfterViewInit{
   title = 'untitled2';
+  login;
+
+
+
+    ngDoCheck() {
+        this.login= localStorage.getItem('dataSource');
+        console.log("ngDoCheck")
+    }
+
+    ngAfterContentInit() {
+        console.log("ngAfterContentInit");
+        this.login= localStorage.getItem('dataSource');
+    }
+
+    ngAfterContentChecked() {
+        this.login= localStorage.getItem('dataSource');
+        console.log("ngAfterContentChecked");
+    }
+
+    ngAfterViewInit() {
+        console.log("ngAfterViewInit");
+        this.login= localStorage.getItem('dataSource');
+    }
+
+    ngAfterViewChecked() {
+        console.log("ngAfterViewChecked");
+        this.login= localStorage.getItem('dataSource');
+    }
+
+    ngOnDestroy() {
+        console.log("ngOnDestroy");
+        this.login= localStorage.getItem('dataSource');
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+constructor(private userservices:UserService,private router:Router){
+  console.log("constroutor");
+    this.login= localStorage.getItem('dataSource');
+    console.log(this.login)
+    this.get();
+}
+ngOnChanges(changes: SimpleChanges): void {
+    this.login= localStorage.getItem('dataSource');
+    console.log("ngChange");
+}
+
+get(){
+    this.login= localStorage.getItem('dataSource');
+}
+    login_(){
+    this.login= localStorage.getItem('dataSource');
+    }
+    ngOnInit(){
+        console.log("nginit");
+        this.login= localStorage.getItem('dataSource');
+        this.get();
+    }
+  logout(){
+      this.login= localStorage.getItem('dataSource');
+    this.userservices.logout().subscribe(data=>data);
+      console.log(this.login)
+      this.get();
+      this.router.navigate(['/welcomePage']);
+      this.get();
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { FormsLoginUserComponent } from './forms-login-user/forms-login-user.component';
+import { FormsRegisterUSerComponent } from './forms-register-user/forms-register-user.component';
+import {UserService} from './user.service';
+import {FormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
+import { FormsEleComponent } from './forms-ele/forms-ele.component';
+import { ApprovalManagerComponent } from './approval-manager/approval-manager.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { StudyEmplyeeComponent } from './study-emplyee/study-emplyee.component';
+import {ElectronicUserService} from './electronic-user.service';
+import { PaidComponent } from './paid/paid.component';
+import { RolesComponent } from './roles/roles.component';
+const routes: Routes =[
 
+    {path:'welcomeApproval',component:ApprovalManagerComponent},
+    {path:'welcomePage',component:WelcomePageComponent},
+    {path:'studyCase',component:StudyEmplyeeComponent},
+    {path: 'welcomeLogin' ,component:FormsLoginUserComponent},
+    {path: 'welcomeRegister',component:FormsRegisterUSerComponent},
+    {path:'welcomeForms',component:FormsEleComponent},
+    {path:'paid',component:PaidComponent},
+    {path:'role',component:RolesComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsLoginUserComponent,
+    FormsRegisterUSerComponent,
+    FormsEleComponent,
+    ApprovalManagerComponent,
+    WelcomePageComponent,
+    StudyEmplyeeComponent,
+    PaidComponent,
+    RolesComponent
   ],
   imports: [
-    BrowserModule
+      NgbModule,
+      BrowserModule,
+      HttpClientModule,
+      FormsModule,
+      RouterModule.forRoot(routes)
+
   ],
-  providers: [],
+  providers: [UserService,ElectronicUserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
